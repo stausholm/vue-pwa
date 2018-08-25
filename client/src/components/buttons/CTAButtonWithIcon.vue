0c3e94f15b5507c8d56ba8dc913966943400c572
Morten Stausholm <muffix26@gmail.com>
2018-08-25 16:01:39 +0200

    Added button styling and CTA button

---
+++ client/src/components/buttons/CTAButtonWithIcon.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="btn btn--cta">
+    <span>
+      <slot name="text">replace me</slot>
+    </span>
+    <i :class="{'btn--cta-pulse': pulse}">
+      <slot name="icon"></slot>
+    </i>
+  </div>  
+</template>
+
+<script>
+export default {
+  name: 'CTAWithIcon',
+  props: {
+    pulse: {
+      type: Boolean,
+      default: true
+    }
+  }
+}
+</script>
+
--- client/src/pages/Example.vue
+++ client/src/pages/Example.vue
@@ -1,11 +1,28 @@
 <template>
   <div>
     <h1>this is example</h1>
+    <cta-button-icon>
+      <template slot="text">amazing cta text</template>
+      <template slot="icon">
+        <icon-base iconName="CTA" width="18" height="18">
+          <icon-beach-access />
+        </icon-base>
+      </template>
+    </cta-button-icon>
   </div>
 </template>
 
 <script>
+import CTAButtonWithIcon from '../components/buttons/CTAButtonWithIcon';
+import IconBase from '../components/icons/IconBase';
+import IconBeachAccess from '../components/icons/IconBeachAccess';
+
 export default {
-  name: 'Example'
+  name: 'Example',
+  components: {
+    'cta-button-icon': CTAButtonWithIcon,
+    IconBase,
+    IconBeachAccess
+  }
 }
 </script>
