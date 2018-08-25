63d8dc0db103be08beb9ffc2fc2da38e33ca7f2c
Morten Stausholm <muffix26@gmail.com>
2018-08-25 16:03:14 +0200

    Added searchbar component

---
+++ client/src/components/search/SearchBar.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <input type="text" placeholder="Search...">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SearchBar',
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
