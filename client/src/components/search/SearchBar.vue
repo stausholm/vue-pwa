7aedef2a5582beba154d3568b7da6675684a35fa
Morten Stausholm <muffix26@gmail.com>
2018-09-08 20:23:12 +0200

    searchBar refactor

--- client/src/components/search/SearchBar.vue
+++ client/src/components/search/SearchBar.vue
@@ -7,36 +7,42 @@
         ref="searchinput"
         v-model="searchVal"
         @input="inputSearch"
-        @keydown.enter="suggestionSelected(searchMatchesSliced[searchHighlightIndex])"
+        @keydown.enter="selectItem"
         @keydown.up="up"
         @keydown.down="down"
         @keydown.esc="cancelSearch"
         @blur="blurSearch"
         @focus="focusSearch">
-        <!-- <button @click="searchForString(searchVal)">
-          SEARCH
-        </button> -->
         <button class="btn-icon">
           <icon-base iconName="search" iconColor="#000" width="24" height="24">
-            <icon-search v-if="showSearch && searchVal == ''"/>
-            <icon-close v-else-if="showSearch && searchVal != ''"/>
+            <icon-search v-if="searchVal == ''" @click.native="bean"/>
+            <icon-close v-else-if="searchVal != ''" @mousedown.native.prevent @click.native="cancelSearch"/>
           </icon-base>
         </button>
     </div>
 
-    <div class="search-suggestions" v-if="showDropdown">
+    <div class="search-suggestions" v-if="showDropdown" ref="suggestions">
       <ul v-if="searchVal">
-        <li v-for="(suggestion, index) in searchMatchesSliced" 
+        <li v-for="suggestion in searchMatchesSliced" 
           :key="suggestion.title"
-          :class="{'search-highlighted-item': index === searchHighlightIndex}"
           @mousedown.prevent
-          @click="suggestionSelected(suggestion)">
+          @click="suggestionSelected(suggestion)"
+          class="js-fetched-suggestion"
+          :data-value="suggestion.title">
           <div class="list-icon">
             <icon-base iconName="search item" iconColor="#000" width="24" height="24">
               <icon-search />
             </icon-base>
           </div>
           <span :inner-html.prop="suggestion.title | highlightmatch(oldVal)"></span>
+        </li>
+        <li @mousedown.prevent @click="searchForString(oldVal)">
+          <p v-if="searchMatches.length > maxResults && searchVal">
+            And {{searchMatches.length - searchMatchesSliced.length}} more results matching <b>{{oldVal}}</b>... 
+          </p>
+          <p v-else>
+            show all matching <b>{{oldVal}}</b>...
+          </p>
         </li>
       </ul>
       <ul v-else-if="recentSearches.length > 0 && showRecents">
@@ -53,10 +59,6 @@
         </li>
         <li @mousedown.prevent @click="clearRecents">Clear recent searches</li>
       </ul>
-      <div class="search-show-all" v-if="searchMatches.length > maxResults && searchVal">
-        <!-- <button class="button button--dark">Show all results ({{searchMatches.length}})</button> -->
-        <p @mousedown.prevent @click="searchForString(oldVal)">And {{searchMatches.length - searchMatchesSliced.length}} more results matching <b>{{oldVal}}</b>... </p>
-      </div>
     </div>
 
     <div class="search-overlay" v-if="showDropdown">
@@ -71,6 +73,8 @@ import IconSearch from '../icons/IconSearch';
 import IconClose from '../icons/IconClose';
 import IconRestore from '../icons/IconRestore';
 
+import debounce from '@/utils/debounce';
+
 export default {
   name: 'SearchBar',
   components: {
@@ -78,7 +82,6 @@ export default {
   },
   data() {
     return {
-      showSearch: true,
       searchVal: '',
       oldVal: '',
       showDropdown: false,
@@ -88,6 +91,7 @@ export default {
       showRecents: true,
       maxRecents: 3,
       recentSearches: [],
+      loading: false,
       dummyData: [
         {
           title: 'Apple',
@@ -125,18 +129,13 @@ export default {
     }
   },
   computed: {
-    navSearchIsOpen() {
-      return this.$store.getters.navSearchIsOpen
-    },
     searchMatches() {
       if (this.updateSearch) {
         return this.dummyData.filter((item) => {
-          // return item.title.toUpperCase().match(this.searchVal.toUpperCase()); // simple version
           return item.title.toUpperCase().match(this.searchVal.toUpperCase().replace(/\s+/g, '.+'));
         })
       } else { // user is navigating the list, so don't update the results to match searchVal
         return this.dummyData.filter((item) => {
-          // return item.title.toUpperCase().match(this.oldVal.toUpperCase()); // simple version
           return item.title.toUpperCase().match(this.oldVal.toUpperCase().replace(/\s+/g, '.+'));
         })
       }
@@ -152,53 +151,35 @@ export default {
       return words.toString().replace(re, function(matchedTxt, a, b) {
         return (`<b class="highlight">${matchedTxt}</b>`);
       });
-
-      // simple version
-      // const iQuery = new RegExp(query, 'ig');
-      // return words.toString().replace(iQuery, function(matchedTxt, a, b) {
-      //   return (`<b class="highlight">${matchedTxt}</b>`);
-      // });
     }
   },
   methods: {
-    openSearch() {
-      this.$store.dispatch('changeNavSearchOpenState', true);
-      this.$nextTick(() => {
-        this.$refs.searchinput.focus();
-      })
+    bean() {
+      console.log('beaan')
     },
-    // openSearch() {
-    //   this.showSearch = true;
-    //   this.$nextTick(() => {
-    //     this.$refs.searchinput.focus();
-    //   });
-    // },
-    // clearSearch() {
-    //   this.searchVal = '';
-    //   this.$refs.searchinput.focus();
-    // },
     inputSearch() {
+      // reset active highlight
       this.searchHighlightIndex = -1;
+      this.handleActive();
+
       this.updateSearch = true;
       this.oldVal = this.searchVal;
     },
     cancelSearch() {
-      // this.showSearch = false;
-      if (this.searchVal === '') this.$refs.searchinput.blur();
+      this.searchVal === '' ? this.$refs.searchinput.blur() : this.$refs.searchinput.focus();
       this.searchVal = '';
-      // this.showDropdown = false;
       this.searchHighlightIndex = -1;
       this.updateSearch = true;
     },
     blurSearch() {
-      // this.showSearch = false;
       this.showDropdown = false;
-      this.$store.dispatch('changeNavSearchOpenState', false);
-      this.$emit('blurSearch')
+      this.$nextTick(() => { // delay the emit to allow navigation default component to hide searchbar before blur event is emittet
+        this.$emit('blurSearch')
+
+      })
     },
     focusSearch() {
       this.showDropdown = true;
-      //this.openSearch();
     },
     searchForString(value) {
       if (value !== '') {
@@ -211,57 +192,104 @@ export default {
       }
     },
     suggestionSelected(suggestion) {
-      // console.log(suggestion)
-      if (this.searchHighlightIndex === -1 && event.type !== 'click') { 
-        return this.searchForString(this.searchVal);
-      }
-
       if (this.showRecents) {
         this.updateRecents(suggestion.title);
       }
 
-      // this.showSearch = false;
       this.searchVal = suggestion.title;
       this.$emit('searchterm', suggestion);
     },
-    up() {
-      event.preventDefault(); // prevent cursor from moving back and forth while using arrow keys
+    selectItem() {
+      let highlightedItem = this.$refs.suggestions.querySelector('.search-highlighted-item');
 
-      if (this.searchVal === '') return false;
+      // reset active highlight;
+      this.searchHighlightIndex = -1;
+      this.handleActive();
+
+      if (highlightedItem != undefined) {
+        //simulate click event on highlighted item
+        highlightedItem.click();
+      } else {
+        // clicked enter with no highlighted item
+        if (this.searchVal) {
+          // input has value, search for that string value
+          this.searchForString(this.searchVal);
+        } 
+        // else do nothing
+      }
+    },
+    up(ev) {
+      ev.preventDefault();
+
+      // prevent navigating if results haven't been fetched yet
+      if (this.loading) return false;
+
+      // don't do anything if no search value is present or any recent searches are present
+      if (this.searchVal === '' && !this.showRecents || this.searchVal === '' && this.recentSearches.length === 0) return false;
 
       if (this.searchHighlightIndex === -1) {
         this.oldVal = this.searchVal; // store searchVal
+        this.searchHighlightIndex = -2;
+        return this.handleActive();
       }
 
-      if (this.searchHighlightIndex > 0) {
-        this.searchHighlightIndex--; // move up one item
-      } else if (this.searchHighlightIndex === 0) {
+      if (this.searchHighlightIndex === 0) {
         this.searchHighlightIndex = -1; // move highlight back into the input field
         this.searchVal = this.oldVal; // restore old search value
       } else {
-        this.searchHighlightIndex = this.searchMatchesSliced.length - 1; // loop back to the last item in the list
+        this.searchHighlightIndex--; // move up one item
       }
+
+      this.handleActive();
 
       this.updateSearch = false; // prevent searchVal from being changed while navigating list
-      if (this.searchHighlightIndex !== -1) {
-        this.searchVal = this.searchMatchesSliced[this.searchHighlightIndex].title; // update searchVal with content from highlighted item
-      }
     },
-    down() {
-      event.preventDefault();
+    down(ev) {
+      ev.preventDefault();
 
-      if (this.searchVal === '') return false;
+      if (this.loading) return false;
+
+      if (this.searchVal === '' && !this.showRecents || this.searchVal === '' && this.recentSearches.length === 0) return false;
 
       if (this.searchHighlightIndex === -1) {
         this.oldVal = this.searchVal;
       }
 
-      if (this.searchHighlightIndex < this.searchMatchesSliced.length - 1) {
-        this.searchHighlightIndex++
-      }
+      this.searchHighlightIndex++;
+
+      this.handleActive();
 
       this.updateSearch = false;
-      this.searchVal = this.searchMatchesSliced[this.searchHighlightIndex].title;
+    },
+    handleActive() {
+      let items = this.$refs.suggestions.querySelectorAll('li');
+      
+      if (this.searchHighlightIndex > items.length - 1) { 
+        // searchHighlightIndex is greater than any element index, so loop back to the first element
+        this.searchHighlightIndex = -1;
+      }
+
+      if (this.searchHighlightIndex === -2) { 
+        // searchHighlightIndex is lower than any element index, so loop around to the last element
+        this.searchHighlightIndex = items.length - 1;
+      }
+
+      items.forEach((item) => {
+        item.classList.remove('search-highlighted-item');
+      })
+      if (items[this.searchHighlightIndex] != undefined) {
+        items[this.searchHighlightIndex].classList.add('search-highlighted-item');
+      }
+
+      let item = this.$refs.suggestions.querySelector('.search-highlighted-item');
+      if (item && item.classList.contains('js-fetched-suggestion')) {
+        // one of the fetched suggestions is highlighted, so update searchVal to contain that value in the input field
+        this.searchVal = item.getAttribute('data-value');
+      } else if (item){
+        // show the original search term 
+        this.searchVal = this.oldVal;
+      }
+      //console.log(items.length, this.searchHighlightIndex)
     },
     updateRecents(value) {
       if (!this.recentSearches.includes(value) && value !== '') {
@@ -279,19 +307,11 @@ export default {
     if (this.showRecents) {
       this.recentSearches = JSON.parse(localStorage.getItem('userRecentSearches')) || [];
     }
+  },
+  watch: {
+    oldVal: debounce(() => {
+      console.log('no updates to oldVal for 1000 miliseconds, make get request and set this.loading to true')
+    }, 1000)
   }
 }
 </script>
-
-<style lang="scss" scoped>
-.search-input-enter {
-  opacity: 0;
-}
-.search-input-enter-active {
-  transition: opacity .2s ease-out;
-}
-.search-input-leave-active {
-  transition: opacity .2s ease-out;
-  opacity: 0;
-}
-</style>
