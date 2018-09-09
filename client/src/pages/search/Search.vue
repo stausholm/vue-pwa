f1ab94a020f6152df0751bc8c4f06daeba43449e
Morten Stausholm <muffix26@gmail.com>
2018-09-09 21:52:21 +0200

    search page react to route param changes

--- client/src/pages/search/Search.vue
+++ client/src/pages/search/Search.vue
@@ -1,41 +1,44 @@
 <template>
-  <div class="container--content">
-    <div v-if="loading">
-      <loader />
-    </div>
-    <div v-else-if="results.length > 0">
-      <div class="search-header">
-        <p>{{results.length}} results for <b>{{query}}</b></p>
-        <div class="search-header__filters">
-          <p>sorting and filtering controls here</p>
-        </div>
+  <div>
+
+    <div class="container--content">
+      <div v-if="loading">
+        <loader />
       </div>
-      <div class="search__results-highlighted">
-        <b>Other users really like these things when it comes to "{{query}}"</b>
-        <div>
-          <div class="card" v-for="(result, index) in highlightedResults" :key="index">
-            <p>{{result.title}}</p>
+      <div v-else-if="results.length > 0">
+        <div class="search-header">
+          <p>{{results.length}} results for <b>{{queryParam}}</b></p>
+          <div class="search-header__filters">
+            <p>sorting and filtering controls here</p>
           </div>
         </div>
+        <div class="search__results-highlighted">
+          <b>Other users really like these things when it comes to "{{queryParam}}"</b>
+          <div>
+            <div class="card" v-for="(result, index) in highlightedResults" :key="index">
+              <p>{{result.title}}</p>
+            </div>
+          </div>
+        </div>
+        <div class="search__results">
+          <article v-for="(result, index) in results" :key="index">
+            <b>{{result.title}}</b>
+            <p>{{result.taste}}</p>
+          </article>
+        </div>
+        <p>Can't find what you're looking for? Try adjusting your search, or explore our extensive list of <router-link to="/explore">categories</router-link></p>
       </div>
-      <div class="search__results">
-        <article v-for="(result, index) in results" :key="index">
-          <b>{{result.title}}</b>
-          <p>{{result.taste}}</p>
-        </article>
+      <div v-else>
+        <h1>Sorry, we couldn't find any results matching "{{queryParam}}"</h1>
+        <p>You can also explore the extensive list of <router-link to="/explore">categories</router-link></p>
+        <p>Try adjusting your search using these suggestions:</p>
+        <ul>
+          <li>Make sure that all words are spelled correctly.</li>
+          <li>Try different keywords.</li>
+          <li>Try more general keywords.</li>
+          <li>Try fewer keywords.</li>
+        </ul>
       </div>
-      <p>Can't find what you're looking for? Try adjusting your search, or explore our extensive list of <router-link to="/explore">categories</router-link></p>
-    </div>
-    <div v-else>
-      <h1>Sorry, we couldn't find any results matching "{{query}}"</h1>
-      <p>You can also explore the extensive list of <router-link to="/explore">categories</router-link></p>
-      <p>Try adjusting your search using these suggestions:</p>
-      <ul>
-        <li>Make sure that all words are spelled correctly.</li>
-        <li>Try different keywords.</li>
-        <li>Try more general keywords.</li>
-        <li>Try fewer keywords.</li>
-      </ul>
     </div>
   </div>
 </template>
@@ -49,7 +52,7 @@ export default {
   },
   data() {
     return {
-      query: this.$route.query.q,
+      //query: this.$route.query.q,
       loading: true,
       results: [],
       dummyData: [
@@ -93,14 +96,22 @@ export default {
       return this.results.filter((item) => {
         return item.taste === 'sweet';
       }).slice(0, 6);
+    },
+    queryParam() {
+      return this.$route.query.q;
+    }
+  },
+  methods: {
+    findMatches() {
+      this.results = this.dummyData.filter((item) => {
+        return item.title.toUpperCase().match(this.queryParam.toUpperCase().replace(/\s+/g, '.+'));
+      })
     }
   },
   created() {
     // if search doesn't come directly from querystring, then search results should be passed over, to avoid an aditional call to db
-    if(this.query) {
-      this.results = this.dummyData.filter((item) => {
-        return item.title.toUpperCase().match(this.query.toUpperCase().replace(/\s+/g, '.+'));
-      })
+    if(this.queryParam) {
+      this.findMatches();
     } else {
       //this.results = this.dummyData;
     }
@@ -108,6 +119,11 @@ export default {
     setTimeout(() => {
       this.loading = false;
     }, 1000);
+  },
+  watch: {
+    queryParam() {
+      this.findMatches();
+    }
   }
 };
 </script>
