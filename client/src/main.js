d542d6a5f32c408932c9ef02dbd917e020881904
Morten Stausholm <muffix26@gmail.com>
2019-04-03 20:48:10 +0200

    add simple logger util

--- client/src/main.js
+++ client/src/main.js
@@ -4,6 +4,8 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+import LoggerService from './utils/logger'
+
 import './styles/index.scss'
 
 import { clickOutside } from './directives/click-outside'
