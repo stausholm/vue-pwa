34782b47fa6c241006538d8af522aba6719b9c83
Morten <muffix26@gmail.com>
2019-07-08 22:53:33 +0200

    search secondary nav styles

--- client/src/pages/search/Search.vue
+++ client/src/pages/search/Search.vue
@@ -3,17 +3,39 @@
     <div class="search-header secondary-header">
       <div class="container">
         <div class="search-input">
-
+          <icon-base class="search-icon" iconName="search" width="24" height="24" aria-hidden="true">
+            <icon-search />
+          </icon-base>
+          <input type="text" placeholder="Search for cool things..">
+          <div class="search-loader" aria-hidden="true" v-if="loading"></div>
+          <button class="filters-button" aria-label="toggle showing search filters" @click="expandFilters = !expandFilters">
+            <icon-base class="" iconName="filter" width="24" height="24" aria-hidden="true">
+              <icon-casino />
+            </icon-base>
+            <span>{{activeFilters.length}}</span>
+          </button>
         </div>
-        <div class="search-filters">
+        <div class="search-filters" v-if="expandFilters">
           <div class="filter">
-
+            <ul class="filter-list">
+              <li class="filter-col" v-for="filterGroup in filters" :key="filterGroup.label">
+                <b class="filter-label">{{filterGroup.label}}</b>
+                <ul class="col-filters">
+                  <li v-for="filter in filterGroup.values" :key="filter.value">
+                    <label>
+                      <input type="checkbox" :value="filter.value" v-model="activeFilters">
+                      {{filter.label}}
+                    </label>
+                  </li>
+                </ul>
+              </li>
+            </ul>
           </div>
-          <div class="sorting">
-            <select name="" id="">
-
+          <!-- <div class="sorting">
+            <select v-model="currentSort">
+              <option v-for="sort in sorting" :key="sort.value" :value="sort.value">{{sort.label}}</option>
             </select>
-          </div>
+          </div> -->
         </div>
       </div>
     </div>
@@ -56,50 +78,31 @@
 
 <script>
 import Loader from '@/components/loader/Loader.vue';
+import IconBase from '@/components/icons/IconBase';
+import IconSearch from '@/components/icons/IconSearch';
+import IconCasino from '@/components/icons/IconCasino';
+
+import {getFilters, getData, getSorting} from './dummydata';
+
 export default {
   name: 'Search',
   components: {
-    Loader
+    Loader,
+    IconBase,
+    IconSearch,
+    IconCasino
   },
   data() {
     return {
       //query: this.$route.query.q,
       loading: true,
       results: [],
-      dummyData: [
-        {
-          title: 'Apple',
-          taste: 'sweet'
-        },
-        {
-          title: 'Banana',
-          taste: 'sweet'
-        },
-        {
-          title: 'Blueberry',
-          taste: 'bad'
-        },
-        {
-          title: 'Kiwi',
-          taste: 'yum'
-        },
-        {
-          title: 'Pear',
-          taste: 'sweet'
-        },
-        {
-          title: 'Pineapple',
-          taste: 'sour'
-        },
-        {
-          title: 'Water melon',
-          taste: 'sweet'
-        },
-        {
-          title: 'How to pineapple',
-          taste: 'paper'
-        }
-      ]
+      dummyData: getData(),
+      filters: getFilters(),
+      sorting: getSorting(),
+      activeFilters: [],
+      currentSort: getSorting()[1].value,
+      expandFilters: false
     }
   },
   computed: {
@@ -142,6 +145,11 @@ export default {
 <style lang="scss" scoped>
 .search-header {
   background-color: gray;
+  padding-top: 2.5em;
+
+  > .container {
+    padding: 0 16px; // default-spacing
+  }
 }
 
 .search__results {
@@ -150,5 +158,139 @@ export default {
     padding: 16px;
     margin-bottom: 1600px;
   }
+}
+
+.filter-list {
+  height: auto;
+  overflow: visible;
+    // clear: both;
+  display: flex;
+  flex-direction: column;
+  // height: 0;
+  // overflow: hidden;
+  padding: 2.5em 0;
+
+  @media screen and (min-width: 960px) {
+    flex-wrap: nowrap;
+  }
+
+  @media screen and (min-width: 550px) {
+    flex-direction: row;
+    flex-wrap: wrap;
+  }
+
+  .filter-col {
+    @media screen and (min-width: 550px) {
+      flex: 0 0 50%;
+    }
+
+    @media screen and (min-width: 768px) {
+      flex: 0 0 33.333333333%;
+    }
+
+    @media screen and (min-width: 960px) {
+      flex: 0 0 20%;
+    }
+
+    .filter-label {
+      letter-spacing: 0.05em;
+      text-transform: uppercase;
+    }
+
+    .col-filters {
+      margin-top: 1em;
+
+      li {
+        margin-bottom: 0.3125em;
+      }
+    }
+  }
+}
+
+.search-input {
+  position: relative;
+
+  .search-icon {
+    opacity: .3;
+    position: absolute;
+    left: 23px;
+    top: 50%;
+    transform: translateY(-50%);
+  }
+
+  input {
+    box-shadow: 0 8px 20px -6px rgba(0,0,0,0.1);
+    outline: none;
+    background-color: white;
+    height: 70px;
+    border-radius: 4px;
+    width: 100%;
+    border: 0;
+    display: block;
+    padding: 15px;
+    padding-left: 55px;
+    color: #444;
+
+    &::placeholder {
+      color: #999;
+      font-style: italic;
+    }
+  }
+
+  .search-loader {
+    position: absolute;
+    right: 15px;
+    top: 0;
+    height: 100%;
+    width: 40px;
+
+    &::before {
+      content: "";
+      position: absolute;
+      background-color: white;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+    }
+
+    &::after {
+      content: "";
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      margin: -10px 0 0 -10px;
+      border-radius: 50%;
+      width: 20px;
+      height: 20px;
+      border: 0.2rem solid rgba(0,0,0,0.15);
+      border-top-color: rgba(0,0,0,0.5);
+      animation: spin .5s infinite linear;
+    }
+  }
+}
+
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+
+  100% {
+    transform: rotate(360deg);
+  }
+}
+
+.filters-button {
+  font-weight: 600;
+  position: absolute;
+  right: 23px;
+  top: 50%;
+  transform: translateY(-50%);
+  border: 0;
+  background-color: #e4e4e4;
+  border-radius: 2px;
+  font-size: 1rem;
+  letter-spacing: 1.5px;
+  line-height: 1;
 }
 </style>
