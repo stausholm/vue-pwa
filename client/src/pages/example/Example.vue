8f26afb6f8f32661cc7a0509ab9a3edd1dad9ac3
Morten <muffix26@gmail.com>
2019-07-24 09:15:38 +0200

    update headings for mobile

--- client/src/pages/example/Example.vue
+++ client/src/pages/example/Example.vue
@@ -7,6 +7,13 @@
         <block-link v-for="link in childRoutes" :key="link.path" :to="'/example/' + link.path">{{link.name}}</block-link>
       </nav>
 
+      <h1>header 1</h1>
+      <h2>header 2</h2>
+      <h3>header 3</h3>
+      <h4>header 4</h4>
+      <h5>header 5</h5>
+      <h6>header 6</h6>
+
       <p style="margin-top:200px">more height!</p>
       <p style="margin-top:200px">more height!</p>
       <search-bar />
